2901e16afdc69077676ec4e3653023de0ab26890
Vaibhav Karve <vaibhavskarve@gmail.com>
2021-02-20 21:45:35 -0600

    use new notation to compactify definition of embeddings

--- src/model.lean
+++ src/model.lean
@@ -171,11 +171,11 @@ instance struc.inhabited {L : lang} : inhabited (struc L) :=
 on the domain and preserves the interpretation of all the symbols of L.-/
 structure embedding {L : lang} (M N : struc L) : Type :=
 (η : M.univ → N.univ)                        -- map of underlying domains
-(η_inj : function.injective η)                -- should be one-to-one
-(η_F : ∀ n f v,                              -- preserves action of each function
-     η (app_vec (M.F (n+1) f) v) = app_vec (N.F (n+1) f) (vector.map η v))
-(η_R : ∀ n r v,                              -- preserves each relation
-     v ∈ (M.R n r) ↔ (vector.map η v) ∈ (N.R n r))
+(η_inj : function.injective η)               -- should be one-to-one
+(η_F : ∀ n (f : L.F (n+1)) (v : vector M.univ (n+1)),
+     η (f^M ⊗ v) = f^N ⊗ vector.map η v)    -- preserves action of each function
+(η_R : ∀ n (r : L.R (n+1)) (v : vector M.univ (n+1)),
+     v ∈ (r̂M) ↔ (vector.map η v) ∈ (r̂N))   -- preserves each relation
 (η_C : ∀ c, η (M.C c) = N.C c)               -- preserves constants
 
 
